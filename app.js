8cd422671290da1296e038fb5b7cd92a1fef9c60
Rocco <roccorighi@hotmail.com>
2015-08-03 21:10:47 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+var request = require('request');
+var challengeUrl = 'http://aerial-valor-93012.appspot.com/challenge';
+
+
+request(challengeUrl, function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    var content = response.body.toString();
+    content = JSON.parse(content);
+    var values = content.values;
+    var token = content.token;
+    calculateSum(values, token);
+  };
+});
+
+var calculateSum = function(values, token) {
+  var sum = 0;
+  values.reduce(function(previousValue, currentValue){
+    sum = previousValue + currentValue;
+    return sum;
+  });
+  answer(sum, token);
+};
+
+var answer = function(sum, token) {
+  var url = challengeUrl + '/' + token + '/' + sum;
+  request(url, function(error, response, body) {
+     if (!error && response.statusCode == 200) {
+      var result = response.body.toString();
+      result = JSON.parse(result);
+      console.log(result.answer);
+    };
+  });
+};
